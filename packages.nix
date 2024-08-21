b05ef2d01ca5112601ac333abeb5dcdce6e4e5c2
bramtechs <bramtech@telenet.be>
2024-08-21 13:29:58 +0200

    format

--- packages.nix
+++ packages.nix
@@ -1,19 +1,13 @@
 let
-pkgs = import (fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/63dacb46bf939521bdc93981b4cbb7ecb58427a0.tar.gz";
+  pkgs = import (fetchTarball {
+    url =
+      "https://github.com/NixOS/nixpkgs/archive/63dacb46bf939521bdc93981b4cbb7ecb58427a0.tar.gz";
     sha256 = "sha256:1lr1h35prqkd1mkmzriwlpvxcb34kmhc9dnr48gkm8hh089hifmx";
   }) { };
-in
-{
-  inherit pkgs; 
+in {
+  inherit pkgs;
   stdenv = pkgs.stdenv;
+  mkShell = pkgs.mkShell;
 
-  inputs = with pkgs; [
-    cmake
-    ninja
-    nlohmann_json
-    tl-expected
-    fmt
-    cxxopts
-  ];
+  inputs = with pkgs; [ cmake nlohmann_json tl-expected fmt cxxopts ];
 }
